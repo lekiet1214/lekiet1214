cb3d7fa85d784145c36906b0b7467fb1b2eb0583
nho <50958432+lekiet1214@users.noreply.github.com>
2024-08-13 08:42:27 +0700

    1 change by Nuxt Studio

--- web/app.config.ts
+++ web/app.config.ts
@@ -1,9 +1,16 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '',
+
   socials: {
     twitter: 'lekiet0101',
     github: 'lekiet1214',
     mastodon: ''
+  },
+
+  nuxtIcon: {
+    iconifyApiOptions: {
+      publicApiFallback: true
+    }
   }
 })
