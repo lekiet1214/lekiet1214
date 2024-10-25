60afb919fb8660ceeafd034b770f47af248349b0
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-10-25 18:55:50 +0700

    configure social links

--- web/app.config.ts
+++ web/app.config.ts
@@ -1,3 +1,12 @@
 export default defineAppConfig({
-  // Configure Content Wind here
+  socials: {
+    twitter: 'https://twitter.com/lekiet0101',
+    github: 'https://github.com/lekiet1214',
+  },
+  nuxtIcon: {
+    aliases: {
+      'dark-mode': 'ph:moon-bold',
+      'light-mode': 'ph:sun-bold'
+    }
+  }
 })
